9124190c081a41829fc0f0735e0fda5a830226e8
Gavin Morris <gavin.morris@health.telstra.com>
2022-10-03 16:21:06 +0800

    use edit/post

--- IboTest.dpr
+++ IboTest.dpr
@@ -8,15 +8,19 @@ uses
 
 var
   conn : TIB_Connection;
-  qry : TIB_Query;
+  qry, lk : TIB_Query;
 
 begin
   try
     conn := TIB_Connection.Create(nil);
+    //conn.Server := 'localhost/3051';
     conn.Database := 'CCARE';
     conn.Username := 'sysdba';                                              
     conn.Password := '******';
     conn.Open;
+
+    lk := TIB_Query.Create(Nil);
+    lk.IB_Connection := conn;
 
     qry := TIB_Query.Create(Nil);
     qry.IB_Connection := conn;
@@ -34,12 +38,17 @@ create table names (
 insert into types (id, name) values (char_to_uuid('AB000000-0000-0000-0000-000000000000'), 'foo');
 *)
 
-    qry.SQL.Text := 'update names set name = :name, type_id = :type_id where id = :id;';
-    qry.Prepare;
-    qry.Params.ByName('id').AsInteger := 1;
-    qry.Params.ByName('name').AsString := 'fred';
-    qry.Params.ByName('type_id').ColData := #$AB#10#0#0#0#0#0#0#0#0#0#0#0#0#0#0;
-    qry.Execute;
+    lk.SQL.text := 'select id, name from types';
+    qry.SQL.Text := 'select id, name, type_id from names';
+
+    lk.open;
+    lk.First;
+
+    qry.open;
+    qry.Edit;
+    qry.FieldByName('name').AsString := 'zeus';
+    qry.FieldByName('type_id').ColData := lk.FieldByName('id').ColData;
+    qry.Post;
 
     Writeln('done!');
     Readln;
